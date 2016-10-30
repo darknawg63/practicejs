67c5a37a4a95ce062e5e9d22a6b078ff082d4ef3
darknawg63@gmail.com <darknawg63@gmail.com>
2016-10-30 15:54:06 +0100

    time 2

--- time/js/time.js
+++ time/js/time.js
@@ -1,6 +1,10 @@
 var date = new Date();
 
+var daylist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var day = date.getDay();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
 
-console.log('Today is : Friday.');
+console.log('Today is: ' + daylist[day] + '.');
 console.log('Current time is : 4 PM : 50 : 22');
